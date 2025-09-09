0c0e420305b5123839c453d20d352bdfc5cc576e
Joao Pedro Marcondes <joaopopm@usp.br>
2025-09-09 19:17:33 -0300

    AAA

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 require 'simplecov'
 
-do
+# Inicia o SimpleCov para monitorar a cobertura de código
+SimpleCov.start 'rails' do
+  # Filtra arquivos que não precisam de cobertura
+  add_filter %w[version.rb initializer.rb]
+
+  # Configuração para rodar no ambiente de CI (Continuous Integration)
   if ENV['CI']
     require 'simplecov-lcov'
 
@@ -9,15 +14,16 @@ do
       c.single_report_path = 'coverage/lcov.info'
     end
 
+    # Define o formatador para gerar o relatório no formato LCOV
     formatter SimpleCov::Formatter::LcovFormatter
   end
-
-  add_filter %w[version.rb initializer.rb]
 end
 
+# O resto do seu arquivo continua aqui...
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
+# ...
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
