f07d1abaffd94d7b990ed3d9de3a65402119cc81
Joao Pedro Marcondes <joaopopm@usp.br>
2025-10-07 21:56:48 -0300

    Adicionei as restrições do modelo Photo

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Storage variant
 gem 'image_processing', '~> 1.14'
 
+gem 'active_storage_validations', '~> 3.0', '>= 3.0.2'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,14 @@
 class Photo < ApplicationRecord
   has_one_attached :foto
+
+
+  validates :foto,
+            attached: true,
+            content_type: {in: ['image/jpeg', 'image/png'], message: 'deve ser JPEG ou PNG'},
+            size: { less_than: 5.megabytes, message: 'deve ser menor que 5MB' }
+  
+
+  validates :latitude, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+  validates :longitude, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+  
 end
