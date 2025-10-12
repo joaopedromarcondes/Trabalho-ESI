17971c2c761dd801dd1f5775554047a15234c928
clebenjr <clebenjuniorcgarcia@hotmail.com>
2025-10-12 15:28:59 -0300

    bd atualizado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_12_145218) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_12_173404) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_12_145218) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "noise_measurements", force: :cascade do |t|
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "photos", force: :cascade do |t|
